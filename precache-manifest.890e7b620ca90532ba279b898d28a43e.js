db7cea12e4b0b8b8a56649aa6f70a1c79d20dd9a
Justin Singh <justin.singh125125@gmail.com>
2019-01-07 21:50:24 -0800

    Updates

---
+++ precache-manifest.890e7b620ca90532ba279b898d28a43e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/fuck/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "81d1b92064773ec39268",
+    "url": "/fuck/static/js/runtime~main.81d1b920.js"
+  },
+  {
+    "revision": "64021d82f6203fc2b3b2",
+    "url": "/fuck/static/js/main.64021d82.chunk.js"
+  },
+  {
+    "revision": "efe2d7cad70b0f47cbb3",
+    "url": "/fuck/static/js/1.efe2d7ca.chunk.js"
+  },
+  {
+    "revision": "64021d82f6203fc2b3b2",
+    "url": "/fuck/static/css/main.f7a22415.chunk.css"
+  },
+  {
+    "revision": "efe2d7cad70b0f47cbb3",
+    "url": "/fuck/static/css/1.f6b69196.chunk.css"
+  },
+  {
+    "revision": "e50e33de461685a59405391c798af646",
+    "url": "/fuck/index.html"
+  }
+];
